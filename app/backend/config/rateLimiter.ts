714444c4da01d3569f0d5f4b220b472df1b8217f
Bertrand <bertrandrigal.pro@gmail.com>
2025-06-24 15:12:40 +0200

    chore: naming and readme udpate

--- app/backend/config/rateLimiter.ts
+++ app/backend/config/rateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const createApiLimiter = rateLimit({
   windowMs: 15 * 60_000, // 15 minutes
-  max: 100, // limit to 100 requests per IP
+  max: 1000, // limit to 1000 requests per IP (increased from 100)
   standardHeaders: true, // return RateLimit-* headers
   legacyHeaders: false, // disable X-RateLimit-* headers
   message: {
