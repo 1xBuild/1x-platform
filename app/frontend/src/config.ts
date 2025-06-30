f4e897f0058a4b93316e1050d5eead0b79463db9
Bertrand <bertrandrigal.pro@gmail.com>
2025-06-30 16:00:58 +0200

    chore: update allie's backend url

--- app/frontend/src/config.ts
+++ app/frontend/src/config.ts
@@ -2,5 +2,5 @@ export const SERVER_URL =
   import.meta.env.VITE_VERCEL_TARGET_ENV === 'production'
     ? 'https://thep33l-production.up.railway.app'
     : import.meta.env.VITE_VERCEL_TARGET_ENV === 'preview'
-      ? 'https://thep33l-staging.up.railway.app'
+      ? 'https://the-p33l-app-backend-allie.up.railway.app'
       : 'http://localhost:8000';
