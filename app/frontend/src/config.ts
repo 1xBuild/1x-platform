ae64c26aeedd4c8df0570da90c612c9bab403c3c
Bertrand <bertrandrigal.pro@gmail.com>
2025-07-05 16:58:31 +0200

    chore: update backend url on frontend

--- app/frontend/src/config.ts
+++ app/frontend/src/config.ts
@@ -1,6 +1,6 @@
 export const SERVER_URL =
   import.meta.env.VITE_VERCEL_TARGET_ENV === 'production'
-    ? 'https://thep33l-production.up.railway.app'
+    ? 'https://thep33l-thep33l.up.railway.app'
     : import.meta.env.VITE_VERCEL_TARGET_ENV === 'preview'
       ? 'https://thep33l-staging.up.railway.app'
       : 'http://localhost:8000';
